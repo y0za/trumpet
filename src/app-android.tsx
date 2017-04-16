4ecf9d62cfea7787be62a4b89c40a556d41146b1
y0za <yoza.is12s@gmail.com>
2017-04-16 23:51:23 +0900

    Rewrite root component with TypeScript

---
+++ src/app-android.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Trumpet
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
