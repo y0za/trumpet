ce62ab1673505716afbf4bad45b23e463a902c16
y0za <yoza.is12s@gmail.com>
2017-04-19 21:27:56 +0900

    Add Api class for accessing to server

---
+++ src/api/index.ts
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+interface Config {
+  headers: any;
+  baseUrl: string;
+  responseType: string;
+}
+
+class Api {
+  private clientId: string;
+  private clientSecret: string;
+  private baseConfig: Config;
+
+  constructor(baseUrl: string, clientId: string, clientSecret: string) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.baseConfig = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      baseUrl: baseUrl,
+      responseType: 'json'
+    };
+  }
+
+  async authenticate(username: string, password: string): Promise<string> {
+    const response = await axios.post('/oauth/token', {
+      'client_id': this.clientId,
+      'client_secret': this.clientSecret,
+      'grant_type': 'password',
+      'username': username,
+      'password': password,
+      'scope': 'read write follow'
+    }, this.baseConfig);
+
+    const accessToken = response.data['access_token'] as string;
+    this.baseConfig.headers['Authorization'] = `Bearer ${accessToken}`;
+    return Promise.resolve(accessToken);
+  }
+
+  get(path: string, config?: Object): Promise<Object> {
+    return axios.get(path, Object.assign({}, this.baseConfig, config));
+  }
+
+  delete(path: string, config?: Object): Promise<Object> {
+    return axios.delete(path, Object.assign({}, this.baseConfig, config));
+  }
+
+  head(path: string, config?: Object): Promise<Object> {
+    return axios.head(path, Object.assign({}, this.baseConfig, config));
+  }
+
+  post(path: string, data: Object, config?: Object): Promise<Object> {
+    return axios.post(path, Object.assign({}, this.baseConfig, config));
+  }
+
+  put(path: string, data: Object, config?: Object): Promise<Object> {
+    return axios.put(path, Object.assign({}, this.baseConfig, config));
+  }
+
+  patch(path: string, data: Object, config?: Object): Promise<Object> {
+    return axios.patch(path, Object.assign({}, this.baseConfig, config));
+  }
+}
