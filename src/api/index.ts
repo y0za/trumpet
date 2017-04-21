bf6cd726851831864038ea75baa9e94e5991263e
y0za <yoza.is12s@gmail.com>
2017-04-21 23:35:46 +0900

    Change Api class to create axios instance and use it

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse } from 'axios';
 
 export class Api {
   private clientId: string;
   private clientSecret: string;
   private baseConfig: AxiosRequestConfig;
+  private client: AxiosInstance;
 
   constructor(baseUrl: string, clientId: string, clientSecret: string) {
     this.clientId = clientId;
@@ -15,44 +16,47 @@ export class Api {
       baseURL: baseUrl,
       responseType: 'json'
     };
+    this.client = axios.create(this.baseConfig)
   }
 
   async authenticate(username: string, password: string): Promise<string> {
-    const response = await axios.post('/oauth/token', {
+    const response = await this.client.post('/oauth/token', {
       'client_id': this.clientId,
       'client_secret': this.clientSecret,
       'grant_type': 'password',
       'username': username,
       'password': password,
       'scope': 'read write follow'
-    }, this.baseConfig);
+    });
 
     const accessToken = response.data['access_token'] as string;
     this.baseConfig.headers['Authorization'] = `Bearer ${accessToken}`;
+    this.client = axios.create(this.baseConfig);
+
     return Promise.resolve(accessToken);
   }
 
   get(path: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axios.get(path, Object.assign({}, this.baseConfig, config));
+    return this.client.get(path, Object.assign({}, this.baseConfig, config));
   }
 
   delete(path: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axios.delete(path, Object.assign({}, this.baseConfig, config));
+    return this.client.delete(path, Object.assign({}, this.baseConfig, config));
   }
 
   head(path: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axios.head(path, Object.assign({}, this.baseConfig, config));
+    return this.client.head(path, Object.assign({}, this.baseConfig, config));
   }
 
   post(path: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axios.post(path, data, Object.assign({}, this.baseConfig, config));
+    return this.client.post(path, data, Object.assign({}, this.baseConfig, config));
   }
 
   put(path: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axios.put(path, data, Object.assign({}, this.baseConfig, config));
+    return this.client.put(path, data, Object.assign({}, this.baseConfig, config));
   }
 
   patch(path: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse> {
-    return axios.patch(path, data, Object.assign({}, this.baseConfig, config));
+    return this.client.patch(path, data, Object.assign({}, this.baseConfig, config));
   }
 }
