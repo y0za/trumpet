fdae490c492b4a8cacf8f74153e37261fa46ec01
y0za <yoza.is12s@gmail.com>
2017-04-22 16:43:19 +0900

    Add updateCurrentUser function

--- src/api/accounts.ts
+++ src/api/accounts.ts
@@ -1,5 +1,5 @@
-import { Request } from './index';
-import { Account } from './entities';
+import { Request, RequestWithData } from './index';
+import { Account, Profile } from './entities';
 
 export function getAccount(get: Request, id: number): Promise<Account> {
   return get(`/api/v1/accounts/${id}`).then((response) => {
@@ -9,6 +9,12 @@ export function getAccount(get: Request, id: number): Promise<Account> {
 
 export function getCurrentUser(get: Request): Promise<Account> {
   return get('/api/v1/accounts/verify_credentials').then((response) => {
+    return response.data as Account
+  })
+}
+
+export function updateCurrentUser(patch: RequestWithData, profile: Profile): Promise<Account> {
+  return patch('/api/v1/accounts/update_credentials', profile).then((response) => {
     return response.data as Account
   })
 }
