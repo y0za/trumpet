9216aa1973559a552ecfacb9719c5e73e52e4bdd
y0za <yoza.is12s@gmail.com>
2017-04-23 20:44:22 +0900

    Add Status and related entities

--- src/api/entities.ts
+++ src/api/entities.ts
@@ -16,9 +16,58 @@ export interface Account {
   header_static: string;
 }
 
+export interface Application {
+  name: string;
+  website: string;
+}
+
+export interface Attachment {
+  id: number;
+  type: string;
+  url: string;
+  remote_url: string;
+  preview_url: string;
+  text_url: string;
+}
+
+export interface Mention {
+  url: string;
+  username: string;
+  acct: string;
+  id: number;
+}
+
 export interface Profile {
   display_name?: string;
   note?: string;
   avatar?: string;
   header?: string;
+}
+
+export interface Status {
+  id: number;
+  uri: string;
+  url: string;
+  account: Account;
+  in_reply_to_id?: number;
+  in_reply_to_account_id?: number;
+  reblog?: Status;
+  content: string;
+  created_at: string;
+  reblog_count: number;
+  favourites_count: number;
+  reblogged?: boolean;
+  favourited?: boolean;
+  sensitive: boolean;
+  spoiler_text: string;
+  visibility: string;
+  media_attachments: Attachment[];
+  mentions: Mention[];
+  tags: Tag[];
+  application: Application;
+}
+
+export interface Tag {
+  name: string;
+  url: string;
 }
