3e84eb2f7f270ed36162164fd015e0d333d6ff43
y0za <yoza.is12s@gmail.com>
2017-05-01 00:02:51 +0900

    Add clearNotifications function

--- src/api/notifications.ts
+++ src/api/notifications.ts
@@ -1,4 +1,4 @@
-import { Request } from './index';
+import { Request, RequestWithData } from './index';
 import { Notification } from './entities';
 import { RangeParams } from './params';
 
@@ -11,5 +11,11 @@ export function getNotifications(get: Request, params?: RangeParams): Promise<No
 export function getNotification(get: Request, id: number): Promise<Notification> {
   return get(`/api/v1/notifications/${id}`).then((response) => {
     return response.data as Notification;
+  });
+}
+
+export function clearNotifications(post: RequestWithData): Promise<void> {
+  return post(`/api/v1/notifications/clear`).then((response) => {
+    return;
   });
 }
