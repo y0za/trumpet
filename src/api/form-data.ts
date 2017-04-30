d66d6b74121025ef999e574b059654ff0b29dcf1
y0za <yoza.is12s@gmail.com>
2017-05-01 00:35:41 +0900

    Add report function

--- src/api/form-data.ts
+++ src/api/form-data.ts
@@ -5,6 +5,12 @@ export interface Profile {
   header?: string;
 }
 
+export interface ReportContent {
+  account_id: number;
+  status_ids: number | number[];
+  comment: string;
+}
+
 export interface Toot {
   status: string;
   in_reply_to_id?: number;
--- src/api/reports.ts
+++ src/api/reports.ts
@@ -1,8 +1,15 @@
-import { Request } from './index';
+import { Request, RequestWithData } from './index';
 import { Report } from './entities';
+import { ReportContent } from './form-data'
 
 export function getReports(get: Request): Promise<Report[]> {
   return get('/api/v1/reports').then((response) => {
     return response.data as Report[];
+  })
+}
+
+export function report(post: RequestWithData, reportContent: ReportContent): Promise<Report> {
+  return post('/api/v1/reports', reportContent).then((response) => {
+    return response.data as Report;
   })
 }
